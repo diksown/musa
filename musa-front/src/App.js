b0c058bf27585e9ea80cd7e42277b2c3d9120d24
Dikson Santos <diksonfer@gmail.com>
2022-04-18 23:55:13 -0300

    make it more react-like

--- musa-front/src/App.js
+++ musa-front/src/App.js
@@ -3,38 +3,56 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-function App() {
+let ActionBar = () => {
   return (
-    <div className="App">
-      <div className="MainCard">
-        <div className="ContentCard">
-          <div className="ProjectTitle">
-            1. MILKIEST
-            <br />
-            2. MOMS ON THE RUN
-            <br />
-            3. MOTHER OF ALL PROJECTS
-          </div>
-          <div className="ProjectDescription">
-            One possible code project about mothers is a Mother's Day Card
-            Generator. This project would involve creating a web page where
-            users could input their mothers' names and some information about
-            them. The web page would then generate a custom Mother's Day card
-            for the user to share with their mother.
-          </div>
-        </div>
-        <div className="ActionBar">
-          <div className="InfoButton">
-            <FontAwesomeIcon icon={faCircleInfo} />
-          </div>
-          <div className="RefreshButton">click to generate</div>
-          <div className="GithubButton">
-            <FontAwesomeIcon icon={faGithub} />
-          </div>
-        </div>
+    <div className="ActionBar">
+      <div className="InfoButton">
+        <FontAwesomeIcon icon={faCircleInfo} />
+      </div>
+      <div className="RefreshButton">click to generate</div>
+      <div className="GithubButton">
+        <FontAwesomeIcon icon={faGithub} />
       </div>
     </div>
   );
-}
+};
+
+let ProjectTitle = ({ content }) => {
+  return <div className="ProjectTitle SpacedText">{content}</div>;
+};
+
+let ProjectDescription = ({ content }) => {
+  return <div className="ProjectDescription SpacedText">{content}</div>;
+};
+
+let ContentCard = ({ title, description }) => {
+  return (
+    <div className="ContentCard">
+      <ProjectTitle content={title} />
+      <ProjectDescription content={description} />
+    </div>
+  );
+};
+
+let mockTitle = "1. MILKIEST\n2. MOMS ON THE RUN\n3. MOTHER OF ALL PROJECTS";
+let mockContent =
+  "One possible code project about mothers is a Mother's Day Card Generator. This project would involve creating a web page where users could input their mothers' names and some information about them. The web page would then generate a custom Mother's Day card for the user to share with their mother.";
+
+let MainCard = () => {
+  return (
+    <div className="MainCard">
+      <ContentCard title={mockTitle} description={mockContent} />
+      <ActionBar />
+    </div>
+  );
+};
+
+let App = () => {
+  return (
+    <div className="App">
+      <MainCard />
+    </div>
+  );
+};
 
 export default App;
