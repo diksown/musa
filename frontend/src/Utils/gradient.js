7a8d0c76913a8650e10122511943ced3f77cf592
Dikson Santos <diksonfer@gmail.com>
2022-05-10 03:14:34 -0300

    add new musa logo! :sparkles:

--- frontend/src/Utils/gradient.js
+++ frontend/src/Utils/gradient.js
@@ -1,25 +1,25 @@
 import chroma from "chroma-js";
 
-const genColor = (hue, chr, lightness) => {
-  return chroma.hcl(hue, chr, lightness).hex();
+const genColor = (hue, chr, luminance) => {
+  return chroma.hcl(hue, chr, luminance).hex();
 };
 
 const generateGradient = () => {
   const randomHue = Math.floor(Math.random() * 360);
-  const deltaHue = 90;
-  const chr = 70;
-  const lightness = 80;
+  const deltaHue = 110;
+  const chr = 40;
+  const luminance = 70;
   return {
-    color1: genColor(randomHue, chr, lightness),
-    color3: genColor(randomHue + deltaHue / 2, chr, lightness),
-    color2: genColor(randomHue + deltaHue, chr, lightness),
+    color1: genColor(randomHue, chr, luminance),
+    color2: genColor(randomHue + deltaHue / 2, chr, luminance),
+    color3: genColor(randomHue + deltaHue, chr, luminance),
   };
 };
 
 const bgGradientStyle = () => {
   const gradient = generateGradient();
   return {
-    backgroundImage: `linear-gradient(120deg, ${gradient.color1}, ${gradient.color3}, ${gradient.color2})`,
+    backgroundImage: `linear-gradient(120deg, ${gradient.color1}, ${gradient.color2}, ${gradient.color3})`,
   };
 };
 
